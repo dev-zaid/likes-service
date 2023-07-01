ce86c10ee7578dd95cfdc0cc37b04b86e7c78403
zaid <zaid.tab123@gmail.com>
2023-07-01 23:10:51 +0530

    🧪 populate DB for testing

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -11,5 +11,27 @@ export default async ({ expressApp }: { expressApp: Express.Application }): Prom
   await express({ app: expressApp });
   Logger.info('✌️ Express loaded');
 
+  if ((await (await (await database()).collection('posts').find().toArray()).length) < 5) {
+    await populatePosts();
+  }
+  Logger.info('✌️ Test data populated');
+
+  Logger.info('Printing all Post ID...');
+  const cursor = await (await database()).collection('posts').find();
+  await cursor.forEach(document => {
+    Logger.info(document._id);
+  });
+
   Logger.info('✅ All modules loaded!');
 };
+
+async function populatePosts() {
+  const testData = [
+    { content: 'Lorem ipsum dolor sit amet' },
+    { content: 'Consectetur adipiscing elit' },
+    { content: 'Sed do eiusmod tempor incididunt' },
+    { content: 'Ut labore et dolore magna aliqua' },
+    { content: 'Ut enim ad minim veniam' },
+  ];
+  await (await database()).collection('posts').insertMany(testData);
+}
